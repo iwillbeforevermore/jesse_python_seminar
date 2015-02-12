1c37f1306fb50324ff8cf3162451d106ac25f755
Jesse Chang <iwillbeforevermore@gmail.com>
2015-02-11 23:04:08 -0500

    add pset 1 first assignment

---
+++ PS1_1.py
@@ -0,0 +1,9 @@
+from visual import *
+sphere1 = sphere(pos = vector(-1,0,0), radius = .5, color = color.green)
+arrow1 = arrow(pos = vector(-1,0,0), axis = vector(+1,+3,0),color = color.red)
+
+sphere2 = sphere(pos = vector(0,0,0), radius = .25, color = color.green)
+arrow2 = arrow(pos = vector(0,0,0), axis = vector(+1,-2,+1),color = color.red)
+
+sphere3 = sphere(pos = vector(1,0,0), radius = .5, color = color.green)
+arrow3 = arrow(pos = vector(1,0,0), axis = vector(-1,-2,0),color = color.red)
