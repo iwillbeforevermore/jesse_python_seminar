5ff19d9fc0ffb8f9a305e0ee435206130d3e751e
Jesse Chang <iwillbeforevermore@gmail.com>
2015-02-11 22:39:27 -0500

    initial commit

---
+++ area.py
@@ -0,0 +1,4 @@
+width = 20
+height = 30
+area = width*height
+print area
---
+++ sphere.py
@@ -0,0 +1,19 @@
+from __future__ import division, print_function
+from visual import *
+charge = sphere()
+charge.radius = 1.5
+charge.pos = vector(0,.2,0)
+charge.color = color.red
+charge.opacity = .5
+charge2 = sphere()
+charge2.radius = 2
+charge2.pos = vector(0,.1,0)
+charge2.color = color.blue
+charge2.opacity = 0.2
+charge3 = sphere()
+charge3.radius = 3
+charge3.pos = vector(0,0,0)
+charge3.color = color.yellow
+charge3.opacity = .1
+
+print (color.green)
