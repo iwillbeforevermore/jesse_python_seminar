ab9db5ff50076a5319c3af4414ea67f09c786d7c
Joecg <joecg@mit.edu>
2015-03-10 17:07:35 -0400

    Lecture_6_in_class_solutions

--- lecture_6/group_6.py
+++ lecture_6/group_6.py
@@ -11,15 +11,12 @@
 #   they can be used to calculate total circuit capacitance.
 from math import pi
 from const import E0
-
 k = 1 / (4 * pi * E0)
 
 class capacitor:
     def __init__(self, capacitance, ref):
         self.capacitance = float(capacitance)               # This will be in Farads
         self.q = 0.0
-        self.ref = ref
-        ref[self] = self.q
 
     def charge(self, voltage):
         self.q = self.capacitance * voltage                 # This will be in Coulombs
@@ -32,13 +29,3 @@ class ll_plate_cap(capacitor):
         self.ref = ref                                      # We can refer to ref as an
         self.capacitance = E0 * area / sep                  #   attribute of self
         self.q = 0.0
-        
-class nestedSphere_cap(capacitor):
-    def __init__(self,radiusA,radiusB,ref):
-        self.radiusA = float(radiusA)
-        self.radiusB = float(radiusB)
-        self.sep = abs(radiusA-radiusB)
-        self.capacitance = E0*pi*4/((1.0/radiusA)-(1.0/radiusB))
-        self.ref = ref
-        self.q = 0.0
-        ref[self] = self.q
