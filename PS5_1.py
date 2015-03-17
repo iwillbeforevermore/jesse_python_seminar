c7907994420e0600227be21face0d3685f3a4f52
Jesse Chang <iwillbeforevermore@gmail.com>
2015-03-16 21:49:16 -0400

    pset 5.1

--- PS5_1.py
+++ PS5_1.py
@@ -1,9 +1,41 @@
 from PS2_1 import *
-from math import sqrt, pi
 import time
 import numpy as np
-E0 = 8.854187817e-12
-k = 1.0/(4*pi*E0)
+from matplotlib.pyplot import plot, show
+from math import sqrt, pi, exp, cos, sin
+from const import E0
 
-a = np.array([0,0,0])
-print a
+k = 1.0 / (4 * pi * E0)
+
+def lamda (theta):
+    return (1E-6)/(1+exp(-5*(theta-pi/2)))
+
+def Phi(P):
+    t = -pi/2
+    circ = pi
+    ring = (np.array([0.0,0.0,0.0]),np.array([0.0,2.0,0.0]))
+##    x = cos(t)
+##    y = sin(t)+1
+##    print x, y
+##    ringParam = np.array([x,y])
+    dt = .1
+    phi = 0.0
+    for i in xrange(int(circ/dt)):
+        dq = lamda(i*dt)*dt
+        x = cos(i+t)
+        y = sin(i+t)+1
+        #print x,y
+        pos = (np.array([x,y,0.0]))
+        r = np.linalg.norm(P-pos)
+        phi += k*dq/r
+    return phi
+
+start = time.time()
+path = range(50)
+V = []
+for j in path:
+    V.append(Phi(np.array([0.01, j / 20.0, j / 30.0])))
+
+plot(path, V)
+print start - time.time()
+show()
